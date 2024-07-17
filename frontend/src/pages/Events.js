194a95c7d80acbc92136ce23dd6a5222cf2b5f4d
TanNguyen <minhtan86a@gmail.com>
2024-07-17 19:54:20 +0700

    339. 6-7 done practice

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "some event",
+  },
+  {
+    id: "e2",
+    title: "event 2",
+  },
+];
+
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default EventsPage;
